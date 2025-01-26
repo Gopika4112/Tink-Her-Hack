06ad1c7c4609f4692ef003066411369254a69120
Gopika4112 <gsgopikasg@gmail.com>
2025-01-26 08:10:13 +0530

    added focus music

--- the_study_cove/src/components/whitesound.js
+++ the_study_cove/src/components/whitesound.js
@@ -2,7 +2,6 @@ import React, { useState, useRef } from "react";
 
 const WhiteNoisePlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(0.5);
   const audioRef = useRef(null);
 
   const togglePlayPause = () => {
@@ -14,60 +13,30 @@ const WhiteNoisePlayer = () => {
     setIsPlaying(!isPlaying);
   };
 
-  const handleVolumeChange = (e) => {
-    const newVolume = e.target.value;
-    setVolume(newVolume);
-    audioRef.current.volume = newVolume;
-  };
-
   return (
-    <div
-      style={{
-        textAlign: "center",
-        margin: "20px auto",
-        padding: "20px",
-        border: "1px solid #ddd",
-        borderRadius: "8px",
-        backgroundColor: "#f9f9f9",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2>White Noise Player</h2>
+    <div>
       <audio
         ref={audioRef}
         loop
-        src="https://www.fesliyanstudios.com/play-mp3/700
-
-
-" // Test with a known working audio source
+        src="https://www.fesliyanstudios.com/play-mp3/700" // Replace with your desired audio source
       ></audio>
-      <div style={{ margin: "10px 0" }}>
-        <button
-          onClick={togglePlayPause}
-          style={{
-            padding: "8px 16px",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-            marginRight: "10px",
-          }}
-        >
-          {isPlaying ? "Pause" : "Play"}
-        </button>
-        <input
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          value={volume}
-          onChange={handleVolumeChange}
-          style={{
-            cursor: "pointer",
-          }}
-        />
-      </div>
+      <button
+        onClick={togglePlayPause}
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          padding: "10px 20px",
+          backgroundColor: "#a97882",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        {isPlaying ? "Pause" : "Focus Music"}
+      </button>
     </div>
   );
 };
