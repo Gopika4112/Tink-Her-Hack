97879caa030b4ddb9b5494297d2a00934ab6dd79
Gopika Sreekumar <151044666+Gopika4112@users.noreply.github.com>
2025-01-26 08:59:25 +0530

    Update App.js

--- the_study_cove/src/App.js
+++ the_study_cove/src/App.js
@@ -5,20 +5,12 @@ import StudyRoom from "./pages/StudyRoom";
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/study-room" element={<StudyRoom />} />
       </Routes>
     </Router>
-=======
-    <div className="App">
-      <HomePage/>
-
-      
-    </div>
->>>>>>> aa86bd2a1d6509a337f89be645839529a2f21147
   );
 }
 
